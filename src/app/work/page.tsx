2182356eae3d5d006baea604dfc9fd4c6a7813ae
Paulo Borges <victorborges296@hotmail.com>
2025-03-12 18:23:12 -0300

    WorkSliderButton component + workPage added

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,4 +1,181 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import React, { useState } from 'react'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+import WorkSliderButtons from '@/components/WorkSliderButtons'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const projects = [
+  {
+    num: '01',
+    category: 'frontend',
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptate eligendi tenetur nemo rerum consequatur.',
+    stack: [{ name: 'Html 5' }, { name: 'Css 3' }, { name: 'Javascript' }],
+    image: '/assets/work/thumb1.png',
+    live: '',
+    github: '',
+  },
+  {
+    num: '02',
+    category: 'frontend',
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptate eligendi tenetur nemo rerum consequatur.',
+    stack: [{ name: 'Html 5' }, { name: 'Css 3' }, { name: 'Javascript' }],
+    image: '/assets/work/thumb2.png',
+    live: '',
+    github: '',
+  },
+  {
+    num: '03',
+    category: 'frontend',
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptate eligendi tenetur nemo rerum consequatur.',
+    stack: [{ name: 'Html 5' }, { name: 'Css 3' }, { name: 'Javascript' }],
+    image: '/assets/work/thumb3.png',
+    live: '',
+    github: '',
+  },
+]
+
 function WorkPage() {
-  return <div>WorkPage</div>
+  const [project, setProject] = useState(projects[0])
+
+  // @ts-ignore
+  function handleSlideChange(swiper) {
+    // get current slide index
+    const currentIndex = swiper.activeIndex
+
+    // update project state based on current slide index
+    setProject(projects[currentIndex])
+  }
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+
+              {/* project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-[#00ff99] transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+
+              {/* project description */}
+              <p className="text-white/60">{project.description}</p>
+
+              {/* stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => (
+                  <li key={index} className="text-xl text-[#00ff99]">
+                    {item.name}
+                    {/* removes the last comma */}
+                    {index !== project.stack.length - 1 && ','}
+                  </li>
+                ))}
+              </ul>
+
+              {/* border */}
+              <div className="border border-white/20" />
+
+              {/* buttons */}
+              <div className="flex items-center gap-4">
+                {/* live project button */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-[#00ff99]" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                {/* github project button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-[#00ff99]" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    {/* overlay */}
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+
+                    {/* image */}
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={project.image}
+                        fill
+                        className="object-cover"
+                        alt=""
+                        quality={100}
+                      />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+
+              {/* slider buttons */}
+              <WorkSliderButtons
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                buttonStyles="bg-[#00ff99] hover:bg-[#00ff99]-hover text-[#1c1c22] text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  )
 }
 export default WorkPage
---
+++ src/components/WorkSliderButtons.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi'
+import { useSwiper } from 'swiper/react'
+
+interface WorkSliderButtonsProps {
+  containerStyles: string
+  buttonStyles: string
+  iconsStyles: string
+}
+
+function WorkSliderButtons({
+  containerStyles,
+  buttonStyles,
+  iconsStyles,
+}: WorkSliderButtonsProps) {
+  const swiper = useSwiper()
+
+  return (
+    <div className={containerStyles}>
+      <button
+        className={buttonStyles}
+        type="button"
+        onClick={() => swiper.slidePrev()}
+      >
+        <PiCaretLeftBold className={iconsStyles} />
+      </button>
+      <button
+        className={buttonStyles}
+        type="button"
+        onClick={() => swiper.slideNext()}
+      >
+        <PiCaretRightBold className={iconsStyles} />
+      </button>
+    </div>
+  )
+}
+export default WorkSliderButtons
